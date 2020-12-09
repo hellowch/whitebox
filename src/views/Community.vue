451ee56f9715fc9bb2272072f36bbf8ab1f755b9
魏陈昊 <525691465@qq.com>
2020-12-09 12:21:51 +0800

    单次添加功能完成

--- src/views/Community.vue
+++ src/views/Community.vue
@@ -3,9 +3,16 @@
         <Header></Header>
         <v-touch v-on:swipeleft="swiperleft"  v-on:swiperight="swiperright" class="v-touch">
 
-           
+            <el-button style="position: fixed;z-index: 1;margin-top: 155%;margin-left: 35%" icon="el-icon-plus" @click="showInput" circle></el-button>
 
             <div class="m-content">
+
+                <el-card style="" class="el-card" :body-style="{ padding: '0px'}" shadow="hover" >
+                    <el-input v-if="inputVisible" v-model="inputValue.title" ref="saveTagInput" @keyup.enter.native="handleInputConfirm" @blur="handleInputConfirm" placeholder="请输入标题"></el-input>
+                    <el-input v-if="inputVisible" v-model="inputValue.text" ref="saveTagInput" @keyup.enter.native="handleInputConfirm" @blur="handleInputConfirm" type="textarea" :rows="2" placeholder="请输入内容"></el-input>
+                </el-card>
+
+
                 <el-row style="margin-bottom: 20px">
                     <el-col :span="30" v-for="(model,key) in models" :key="key">
                         <div class="link-top"></div>
@@ -38,7 +45,6 @@
             </div>
 
 
-
         </v-touch>
         <Footer></Footer>
     </div>
@@ -57,6 +63,21 @@
             return {
                 activeName: 'first',
                 currentDate: new Date(),
+
+                inputVisible: false,
+                inputValue:
+                    {
+                        id: '5',
+                        avatar:"https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png",
+                        username:'我是管理员',
+                        url: 'https://picsum.photos/360/460?random=1',
+                        title: '',
+                        text:'',
+                        data:'刚刚',
+                        comments:'0',
+                        give:'0'
+                    },
+
                 models: [
                     {
                         id: '1',
@@ -69,7 +90,7 @@
                             '我全部见切出去，见切出去了啊！\n' +
                             '\n' +
                             '见切出去自然是按传统狩猎的方式点到为止，大回旋捏在手里放他头前面了啊，放在他头前面，没打他。\n',
-                        data:'58分钟前',
+                        data:'30分钟前',
                         comments:'112',
                         give:'50'
                     },
@@ -80,7 +101,7 @@
                         url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1607432754345&di=6911e988bb0638104ccfd4f48ffb5a7a&imgtype=0&src=http%3A%2F%2Fpic.16pic.com%2F00%2F54%2F22%2F16pic_5422967_b.jpg',
                         title: "怎么连接gog",
                         text:'哪位hxd知道怎么连接gog跟steam吗？',
-                        data:'58分钟前',
+                        data:'45分钟前',
                         comments:'112',
                         give:'50'
                     },
@@ -102,7 +123,7 @@
                         url: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1892726360,3154488886&fm=26&gp=0.jpg',
                         title: "有没有适合定制鼠标垫的图片",
                         text:'桌子贴了桌纸，而且这个桌纸很容易被破坏，每次我冲一杯热的东西后桌纸总会留下痕迹，由于我每次底下都垫一块布，所以痕迹不是很明显，有一次忘记垫了然后留下了特别明显的痕迹，而且桌纸在我日常玩游戏时被我不小心弄出两个小口。所以想买一个大鼠标垫把桌面覆盖住。',
-                        data:'58分钟前',
+                        data:'1小时前',
                         comments:'112',
                         give:'50'
                     },
@@ -110,13 +131,30 @@
                 ]
             };
         },
+
         methods: {
             swiperleft: function () {  //右划切换到goods页
                 this.$router.push({'path':'/games'});
             },
             swiperright: function () { //左滑切换到detail页
                 this.$router.push({'path':'/home'});
-            }
+            },
+
+            showInput() {
+                this.inputVisible = true;
+                this.$nextTick(_ => {
+                    this.$refs.saveTagInput.$refs.input.focus();
+                });
+            },
+
+            handleInputConfirm() {
+                let inputValue = this.inputValue;
+                if (inputValue) {
+                    this.models.unshift(inputValue);
+                }
+                this.inputVisible = false;
+                this.inputValue = '';
+            },
 
         }
     }
